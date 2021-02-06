218041870c42e7a38671b007d5aa17dd9476d181
HugoPorto <victor.almeida.porto7@gmail.com>
2021-02-06 03:44:38 -0300

    First commit

---
+++ src/main/java/me/medical/WsmedicalApplication.java
@@ -0,0 +1,13 @@
+package me.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsmedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/medical/WsmedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsmedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
